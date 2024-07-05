a32a8df7e90fea398396f1056a83177ea46ee298
Vishal Sharma <785vishal443@gmail.com>
2024-07-05 22:44:51 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  return <button id="otpButton">Send OTP</button>;
+  const [timer , setTimer] = useState(5)
+  function handleClick() {
+    setTimer(prev=>prev-1) // first subtract
+      const timerId = setInterval(()=>{
+        setTimer((prev)=> {
+          if(prev ==0) {
+            clearInterval(timerId)
+            return 5
+          }
+          return prev -1
+        })
+      }, 1000)
+  }
+  return <button id="otpButton" onClick={handleClick}>{timer ===5 ? 'Send Otp': timer}</button>;
 }
 
 export default App;
